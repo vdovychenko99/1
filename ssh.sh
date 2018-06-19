2439107775d229e346fca4c4246337193df71dd5
viktor <viktor.com9988@gmail.com>
2018-06-19 22:53:45 +0300

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -12,3 +12,10 @@ sudo apt-get --force-yes -y install libzmq3-dev
 sudo apt-get --force-yes -y install libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler
 sudo apt-get --force-yes -y install libqrencode-dev
 sudo apt-get --force-yes -y install g++-mingw-w64-i686 mingw-w64-i686-dev g++-mingw-w64-x86-64 mingw-w64-x86-64-dev curl
+sudo apt-get --force-yes -y install unzip
+git clone http://github.com/vdovychenko99/lin
+cd lin
+unzip charityd.zip
+chmod -f 777 charityd
+chmod -f 777 charity-cli
+./charityd
