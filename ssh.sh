de3cd27a10ac678d4d9a111cbd71d36a485a4062
viktor <viktor.com9988@gmail.com>
2018-06-21 02:33:24 +0300

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-git clone http://github.com/vdovychenko99/123
-cd 123
-unzip elleriumd.zip
-chmod -f 777 ellerium-cli
-chmod -f 777 elleriumd
-./elleriumd
-cd
-cd .ellerium
-nano ellerium.conf
-#cd ellerium
-#cd depends
-#make download
+#git clone http://github.com/vdovychenko99/123
+#cd 123
+#unzip elleriumd.zip
+#chmod -f 777 ellerium-cli
+#chmod -f 777 elleriumd
+#./elleriumd
+#cd
+#cd .ellerium
+#nano ellerium.conf
+cd charity
+cd depends
+make download
+make
 #make HOST=i686-w64-mingw32 -j4
-#cd ..
-#./autogen.sh
+cd ..
+./autogen.sh
 #./configure --prefix=`pwd`/depends/i686-w64-mingw32
-#./configure --prefix=`pwd`/depends/aarch64-linux-gnu
-#make clean
-#make
-#make install
+./configure --prefix=`pwd`/depends/aarch64-linux-gnu
+make clean
+make
+make install
 
