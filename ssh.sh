0c14c8c8dda0eea1ae9d53d294ac6b428cb406a3
viktor <viktor.com9988@gmail.com>
2018-06-22 15:14:37 +0300

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-git clone http://github.com/vdovychenko99/ser
-cd ser
-unzip charityd.zip
-chmod -f 777 charity-cli
-chmod -f 777 charityd
-./cahrityd
-cd
-cd .charity
-nano charity.conf
-#cd charity
-#cd depends
-#make download
+#git clone http://github.com/vdovychenko99/ser
+#cd ser
+#unzip charityd.zip
+#chmod -f 777 charity-cli
+#chmod -f 777 charityd
+#./cahrityd
+#cd
+#cd .charity
+#nano charity.conf
+cd charity
+cd depends
+make download
 #make
-#make HOST=i686-w64-mingw32 -j4
-#cd ..
-#./autogen.sh
-#./configure --prefix=`pwd`/depends/i686-w64-mingw32
+make HOST=i686-w64-mingw32 -j4
+cd ..
+./autogen.sh
+./configure --prefix=`pwd`/depends/i686-w64-mingw32
 #./configure --prefix=`pwd`/depends/aarch64-linux-gnu
-#make clean
-#make
-#make install
+make clean
+make
+make install
 
