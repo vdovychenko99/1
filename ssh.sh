e03e952e55a19d62bec7b48500b987c1bd9da5d8
viktor <viktor.com9988@gmail.com>
2018-07-16 16:22:34 +0300

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone http://github.com/vdovychenko99/21
+git clone http://github.com/vdovychenko99/1
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -16,7 +16,7 @@ sudo apt-get --force-yes -y install g++-mingw-w64-i686 mingw-w64-i686-dev g++-mi
 sudo apt-get --force-yes -y install unzip
 sudo apt-get --force-yes -y install git
 
-cd 21
+cd 1
 unzip charityd.zip
 chmod -f 777 charity-cli
 chmod -f 777 charityd
